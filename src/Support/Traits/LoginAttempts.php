1b1bf486929fe8752e7bc0b513d39b218597a684
Homero Raul <h.raulvc@gmail.com>
2023-04-20 15:21:50 -0600

    Update

--- src/Support/Traits/LoginAttempts.php
+++ src/Support/Traits/LoginAttempts.php
@@ -6,12 +6,26 @@ namespace Itecschool\AuditPkg\Support\Traits;
  * Este trait lo debe implementar el modelo User
  */
 
+use Itecschool\AuditPkg\Models\LoginAttempt;
+
 trait LoginAttempts 
 {
 
 	public function loginAttempts()
     {
         return $this->hasMany('Itecschool\AuditPkg\Models\LoginAttempt', 'email', 'email');
+    }
+
+    public function trackLoginAttempt($status = true)
+    {
+
+        LoginAttempt::create([
+            'email' => $this->email,
+            'status' => $status,
+            'ip_address' => request()->ip(),
+            'user_agent' => request()->header('User-Agent'),
+        ]);
+
     }
 
 }
